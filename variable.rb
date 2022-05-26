fc1c6e734eea455a13d3a31816279e5a64214893
github_sabassalgado <sabas.salgado@gmail.com>
2022-05-26 12:42:18 -0400

    agregando ejemplo de variable en ruby

---
+++ variable.rb
@@ -0,0 +1,13 @@
+x=10
+y=20
+suma = x + y
+puts suma
+
+precio_dolar= 20
+peso_mx= 10
+
+conversion = precio_dolar * peso_mx
+puts conversion
+texto_saludo="hola desde mexico"
+puts texto_saludo
+puts "hasta el proximo codigo"
