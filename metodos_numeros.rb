1531cc8b2fcb7e540e95740842238524767a5171
github_sabassalgado <sabas.salgado@gmail.com>
2022-05-27 10:08:05 -0400

    agregando archivo metodos

---
+++ metodos_numeros.rb
@@ -0,0 +1,22 @@
+
+numero_entero=1776
+#metodo para numeros enteros siguiente o succesor
+puts numero_entero.next
+puts numero_siguiente=numero_entero.succ
+#metod para validar si un numero es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+#conversion de entero a flotante
+puts numero_siguiente/numero_entero.to_f
+puts 800/270.to_f
+#longitud de un numero
+print 356.digits
+print 78905.digits
+#redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
+
---
+++ numbers.rb
@@ -0,0 +1,19 @@
+#integer
+numero1=20
+numero2=30
+la_suma= numero1 + numero2
+puts la_suma
+puts numero1.class 
+puts numero2.class
+puts la_suma.class
+
+num_flotante1= 2.13
+num_flotante2= 7.67
+otra_suma= num_flotante1 + num_flotante2
+puts num_flotante1.class
+puts num_flotante2.class
+puts otra_suma.class
+suma_final= la_suma + otra_suma
+puts suma_final
+puts suma_final.class
+
