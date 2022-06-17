a8bbc23e181d84e795f32bab4d8b7a31966be46e
sabassalgado <sabas.salgado@gmail.com>
2022-06-17 10:13:28 -0400

    generador_palabra

---
+++ animal2/classes/animals.rb
@@ -0,0 +1,11 @@
+class animals
+attr_reader :noise    
+    def noise 
+        @noise
+    end
+    # def noise=(value)
+    #     @noise = value
+    # end
+
+end
+
---
+++ animal2/init.rb
@@ -0,0 +1,12 @@
+
+require_relative "classes/animals"
+
+chanchito = Animal.new
+chanchito.noise = "Oink"
+puts chanchito.noise
+
+
+
+
+
+object.full_name("Rocosso","Orson")
